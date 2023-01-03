948c764f5119af29d8582b75126e971111de7e47
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-01-03 07:10:53 -0300

    opinahub script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.min.css';
 import { AppProps } from 'next/app';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { ThemeProvider } from 'styled-components';
 
 import { RootProvider } from '~/hooks';
@@ -20,7 +20,13 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
         href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
         rel="stylesheet"
       />
+
+      <link
+        rel="stylesheet"
+        href="https://app.opinahub.com.br/script/main.css"
+      />
     </Helmet>
+
     <ThemeProvider theme={theme}>
       <RootProvider>
         <ToastContainer />
@@ -28,6 +34,12 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
         <Component {...pageProps} />
       </RootProvider>
     </ThemeProvider>
+
+    <div
+      id="app-opinahub"
+      data-opinahub="939e35cb-f2f3-44d2-a9a6-169f1597d1a0"
+    />
+    <script type="module" src="https://app.opinahub.com.br/script/main.js" />
   </>
 );
 
